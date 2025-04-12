233cbf89ef10a83fc6abd536220e6577638c5ed9
m.babushkin <m.babushkin@tbank.ru>
2025-04-12 15:57:30 +0500

    delete: removed tests

--- common/sculptor-contract/src/commonTest/kotlin/org/alexcawl/sculptor/common/contract/ModifierContractTest.kt
+++
@@ -1,9 +0,0 @@
-package org.alexcawl.sculptor.common.contract
-
-import kotlinx.serialization.KSerializer
-
-abstract class ModifierContractTest<MC : ModifierContract> : ContractTest<MC>() {
-    abstract override val value: MC
-    abstract override val serializer: KSerializer<MC>
-    abstract override val string: String
-}
--- common/sculptor-contract/src/commonTest/kotlin/org/alexcawl/sculptor/common/contract/StateContractTest.kt
+++
@@ -1,9 +0,0 @@
-package org.alexcawl.sculptor.common.contract
-
-import kotlinx.serialization.KSerializer
-
-abstract class StateContractTest<SC : StateContract> : ContractTest<SC>() {
-    abstract override val value: SC
-    abstract override val serializer: KSerializer<SC>
-    abstract override val string: String
-}
