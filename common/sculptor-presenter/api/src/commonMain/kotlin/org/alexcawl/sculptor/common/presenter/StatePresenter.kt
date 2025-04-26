106631463934414774e99c6c06b727a8168056b1
m.babushkin <m.babushkin@tbank.ru>
2025-04-26 17:11:24 +0500

    docs: добавление документации к интерфейсам и классам презентера
    Добавлена документация для классов и интерфейсов, связанных с презентерами, включая `Presenter`, `PresenterScope`, `PresenterProvider`, `StateValidator`, `ComponentProvider`, расширения `PresenterScope`, `StatePresenter` и `ModifierPresenter`. Документация описывает назначение каждого класса и интерфейса, а также параметры и возвращаемые значения методов.

--- common/sculptor-presenter/api/src/commonMain/kotlin/org/alexcawl/sculptor/common/presenter/StatePresenter.kt
+++ common/sculptor-presenter/api/src/commonMain/kotlin/org/alexcawl/sculptor/common/presenter/StatePresenter.kt
@@ -4,6 +4,17 @@ import org.alexcawl.sculptor.common.contract.StateContract
 import org.alexcawl.sculptor.common.layout.UiState
 import kotlin.reflect.KClass
 
+/**
+ * Abstract presenter class that handles state management based on the [StateContract].
+ * This class extends the [Presenter] class and is designed to work with input states
+ * that implement or extend the [StateContract] and output states represented by [UiState].
+ *
+ * @param Input The type of input state that extends or implements [StateContract].
+ */
 public abstract class StatePresenter<Input : StateContract> : Presenter<Input, UiState>() {
+    /**
+     * The output state class type, which is fixed to [UiState].
+     * This property ensures that all subclasses of [StatePresenter] will output states of type [UiState].
+     */
     public final override val output: KClass<UiState> = UiState::class
 }
--- common/sculptor-presenter/impl/src/commonMain/kotlin/org/alexcawl/sculptor/common/presenter/PresenterScopeFactory.kt
+++ common/sculptor-presenter/impl/src/commonMain/kotlin/org/alexcawl/sculptor/common/presenter/PresenterScopeFactory.kt
@@ -2,10 +2,22 @@ package org.alexcawl.sculptor.common.presenter
 
 import org.alexcawl.sculptor.common.presenter.impl.PresenterScopeImpl
 
+/**
+ * Factory class responsible for creating instances of [PresenterScope].
+ *
+ * @property presenterProvider Provides instances of presenters.
+ * @property stateValidator Validates the state of the presenter.
+ */
 public abstract class PresenterScopeFactory(
     private val presenterProvider: PresenterProvider,
     private val stateValidator: StateValidator,
 ) {
+    /**
+     * Creates a new instance of [PresenterScope] using the provided [ComponentProvider].
+     *
+     * @param componentProvider Provides necessary components for the presenter scope.
+     * @return A new instance of [PresenterScope].
+     */
     public fun create(componentProvider: ComponentProvider): PresenterScope {
         return PresenterScopeImpl(
             presenterProvider = presenterProvider,
