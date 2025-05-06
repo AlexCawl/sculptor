674a095a5c7235a9b9735720dedca89cd4bb95f9
m.babushkin <m.babushkin@tbank.ru>
2025-05-06 20:07:12 +0500

    docs: добавлены комментарии к интерфейсу UiState и классу Layout

--- core/layout/src/commonMain/kotlin/org/alexcawl/sculptor/core/layout/Layout.kt
+++ core/layout/src/commonMain/kotlin/org/alexcawl/sculptor/core/layout/Layout.kt
@@ -3,6 +3,13 @@ package org.alexcawl.sculptor.core.layout
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
 
+/**
+ * Represents a layout configuration for a UI component.
+ *
+ * @property id A unique identifier for the layout.
+ * @property modifier A [Modifier] that can be used to adjust the layout's behavior and appearance.
+ * @property uiState The current state of the UI component, encapsulated in a [UiState] object.
+ */
 @Immutable
 public data class Layout(
     public val id: String,
--- core/layout/src/commonMain/kotlin/org/alexcawl/sculptor/core/layout/UiState.kt
+++ core/layout/src/commonMain/kotlin/org/alexcawl/sculptor/core/layout/UiState.kt
@@ -2,5 +2,11 @@ package org.alexcawl.sculptor.core.layout
 
 import androidx.compose.runtime.Stable
 
+/**
+ * Represents the state of the user interface.
+ *
+ * This interface is marked with the [Stable] annotation to indicate that instances of implementing classes
+ * can be safely used in Compose's remember mechanism without causing unnecessary recompositions.
+ */
 @Stable
 public interface UiState
